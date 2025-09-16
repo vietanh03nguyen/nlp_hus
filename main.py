9c2969957b6c8c1bbd4de961285f38d2541f1b9b
VietAnh <vietanh03nguyen@gmail.com>
2025-09-16 14:54:17 +0700

    Added Regex Tokenizer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from src.preprocessing.simple_tokenizer import SimpleTokenizer
+from src.preprocessing.regex_tokenizer import RegexTokenizer
 
 if __name__ == "__main__":
     txt1 = "Hello, world! This is a test."
@@ -6,5 +7,13 @@ if __name__ == "__main__":
     txt3 = "Let's see how it handles 123 numbers and punctuation!"
     
     simple_tokenizer = SimpleTokenizer()
+    regex_tokenizer = RegexTokenizer()
+    
     
     print("simple tokenizer txt1:", simple_tokenizer.tokenize(txt1))
+    print("simple tokenizer txt1:", simple_tokenizer.tokenize(txt2))
+    print("simple tokenizer txt1:", simple_tokenizer.tokenize(txt3))
+    
+    print("simple tokenizer txt1:", regex_tokenizer.tokenize(txt1))
+    print("simple tokenizer txt1:", regex_tokenizer.tokenize(txt2))
+    print("simple tokenizer txt1:", regex_tokenizer.tokenize(txt3))
--- src/preprocessing/regex_tokenizer.py
+++ src/preprocessing/regex_tokenizer.py
@@ -1,1 +1,6 @@
-from 
+from src.core.interfaces import Tokenizer
+import re
+
+class RegexTokenizer(Tokenizer):
+    def tokenize(self, text):
+        return re.findall("\w+|[^\w\s]", text) 
